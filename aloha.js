185fe5ebca8b03250bf631627403dffdbcb3eaea
aleotoidayy <luannguyen6695@gmail.com>
2025-06-19 14:38:42 +0700

    Update aloha.js

--- aloha.js
+++ aloha.js
@@ -1,3 +1,7 @@
+// Author: aleotoidayy
+// Date: 2025-06-01
+// Purpose: Aloha Vpn Premium
+
 let obj = JSON.parse($response.body);
 obj.profile.is_premium = true,
 obj.profile.end_of_premium = 4080979200,
