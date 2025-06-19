cd287d654f2929a5cdf36c9ded11341caa98df5a
aleotoidayy <luannguyen6695@gmail.com>
2025-06-19 14:50:18 +0700

    Update spotify-json.js

--- spotify-json.js
+++ spotify-json.js
@@ -1,9 +1,13 @@
+// Author: aleotoidayy
+// Date: 2025-06-01
+// Purpose: Spotify Premium
+
 console.log(`spotify-json-2023.06.13`);
 let url = $request.url;
-// console.log(`原始url:${url}`);
+
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
-    // console.log(`替换platform:${url}`);
+    
 } else {
     console.log('无需处理');
 }
