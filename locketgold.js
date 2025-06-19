fc0457cedd6125faeabc7b0df84d2bcbbb68bf5a
aleotoidayy <luannguyen6695@gmail.com>
2025-06-19 14:46:52 +0700

    Update locketgold.js

--- locketgold.js
+++ locketgold.js
@@ -1,4 +1,7 @@
-// === locketgold.js ===
+// Author: aleotoidayy
+// Date: 2025-06-01
+// Purpose: Locket Gold
+
 const mapping = {
   '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
   'Locket': ['Gold']
@@ -6,7 +9,7 @@ const mapping = {
 
 var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="hihi";var locket02={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2009-08-17T01:04:18Z",purchase_date:"2009-08-17T01:04:17Z",store:"app_store"},locket01={grace_period_expires_date:null,purchase_date:"2009-08-17T01:04:17Z",product_identifier:"com.locket02.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(locket01.product_identifier=s,obj.subscriber.subscriptions[s]=locket02):obj.subscriber.subscriptions["com.locket02.premium.yearly"]=locket02,obj.subscriber.entitlements[e]=locket01}else obj.subscriber.subscriptions["com.locket02.premium.yearly"]=locket02,obj.subscriber.entitlements.pro=locket01;$done({body:JSON.stringify(obj)});
 
-// === locket.js ===
+
 var request = $request;
 
 const options = {
@@ -58,7 +61,7 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
       "product_identifier": productIdentifier
     };
 
-    // Add product identifier to subscriptions
+     
     jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
       "expires_date": "9692-01-01T01:01:01Z",
       "original_purchase_date": "2009-08-17T01:01:01Z",
