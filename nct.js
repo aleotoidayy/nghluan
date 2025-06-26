010486e5cdab12a4f9ec86834acf95b3636dd3be
aleotoidayy <luannguyen6695@gmail.com>
2025-06-26 19:38:29 +0700

    Delete nct.js

--- nct.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-let url = $request.url;
-var cons = "users/info";
-if(url.indexOf(cons) != -1)
-{
-obj.data.VIPExpire= "1/01/2099 00:00:00";
-obj.data.isVIP= true;
-}
-$done({body: JSON.stringify(obj)});
