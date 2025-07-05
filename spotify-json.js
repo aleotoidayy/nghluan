bfbc1df4ef0766d1526f26787231779a4d19b8b8
aleotoidayy <luannguyen6695@gmail.com>
2025-07-05 14:11:07 +0700

    Create spotify-json.js

---
+++ spotify-json.js
@@ -0,0 +1,15 @@
+console.log(`spotify-json-2025.06.25`);
+let url = $request.url;
+
+if (url.includes('com:443')) {
+    url = url.replace(/com:443/, 'com');
+}
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+    
+} else {
+    console.log('无需处理');
+}
+$done({
+    url
+});
