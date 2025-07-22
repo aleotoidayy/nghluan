bd170a9456b462f034294b73ae5a58b6d02e6432
aleotoidayy <luannguyen6695@gmail.com>
2025-07-22 21:41:58 +0700

    Update lightroom.js

--- lightroom.js
+++ lightroom.js
@@ -1,7 +1,3 @@
-// Author: aleotoidayy
-// Date: 2025-06-01
-// Purpose: Lightroom
-
 body = $response.body.replace(/while.{7}\n/, "");
 let obj = JSON.parse(body);
 obj.entitlement.status="subscriber";
