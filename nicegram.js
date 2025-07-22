31598c2e2d6c572b93a7c9cbac28820661242205
aleotoidayy <luannguyen6695@gmail.com>
2025-07-22 21:43:55 +0700

    Update nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -1,7 +1,3 @@
-// Author: aleotoidayy
-// Date: 2025-06-01
-// Purpose: NiceGram Premium
-
 var Q = JSON.parse($response.body);
 Q.data.user.lifetime_subscription = true;
 Q.data.user.store_subscription = true;
