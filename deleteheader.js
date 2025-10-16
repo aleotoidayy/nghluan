9b4c717ff855dbdecadf1d298bcc70c8012448e4
aleotoidayy <luannguyen6695@gmail.com>
2025-10-16 20:40:45 +0700

    Update deleteheader.js

--- deleteheader.js
+++ deleteheader.js
@@ -1,4 +1,17 @@
-const version = 'V1.0.2';
+Const version = 'V1.0.3';
 
 
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+function removeHeader(headers, key) {
+    const lowerKey = key.toLowerCase();
+    if (headers[key]) {
+        delete headers[key];
+    }
+    if (headers[lowerKey]) {
+        delete headers[lowerKey];
+    }
+}
+var modifiedHeaders = $request.headers;
+removeHeader(modifiedHeaders, "If-None-Match");
+removeHeader(modifiedHeaders, "X-RevenueCat-ETag");
+removeHeader(modifiedHeaders, "X-Headers-Hash");
+$done({headers: modifiedHeaders});
