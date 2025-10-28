2ddcfbeffe6b0a2a2580ad5c703e38d5f5b6528f
aleotoidayy <luannguyen6695@gmail.com>
2025-10-28 17:09:29 +0700

    Update voxcut.js

--- voxcut.js
+++ voxcut.js
@@ -1,7 +1,6 @@
 var aleoo = JSON.parse($response.body); 
 aleoo.subscriber.entitlements = {  
   "premium": {
-    "expires_date": "2099-01-01T09:09:09Z",
     "product_identifier": "com.zafar.voxcut.lifetime",
     "purchase_date": "2025-01-01T02:33:33Z"
   }
@@ -9,7 +8,6 @@ aleoo.subscriber.entitlements = {
 aleoo.subscriber.original_purchase_date = "2025-01-01T02:33:33Z"; 
 aleoo.subscriber.subscriptions = {          
   "com.zafar.voxcut.lifetime": {
-    "expires_date": "2099-01-01T09:09:09Z",
     "original_purchase_date": "2025-01-01T02:33:33Z",
     "purchase_date": "2025-01-01T02:33:33Z",
     "ownership_type" : "PURCHASED",
