646acd9aaaab98a7422ab30cb08d9fd2f6cbfb7c
aleotoidayy <luannguyen6695@gmail.com>
2025-10-28 17:14:50 +0700

    Update subtracky.js

--- subtracky.js
+++ subtracky.js
@@ -1,7 +1,6 @@
 var aleoo = JSON.parse($response.body); 
 aleoo.subscriber.entitlements = {  
   "premium": {
-    "expires_date": "2099-01-01T09:09:09Z",
     "product_identifier": "premium_subtracky_lifetime",
     "purchase_date": "2025-01-01T02:33:33Z"
   }
@@ -9,7 +8,6 @@ aleoo.subscriber.entitlements = {
 aleoo.subscriber.original_purchase_date = "2025-01-01T02:33:33Z"; 
 aleoo.subscriber.subscriptions = {          
   "premium_subtracky_lifetime": {
-    "expires_date": "2099-01-01T09:09:09Z",
     "original_purchase_date": "2025-01-01T02:33:33Z",
     "purchase_date": "2025-01-01T02:33:33Z",
     "ownership_type" : "PURCHASED",
