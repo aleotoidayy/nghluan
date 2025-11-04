eef9c604fa75860dec7a11d083d2e2f7aa647a31
aleotoidayy <luannguyen6695@gmail.com>
2025-11-04 14:08:23 +0700

    Create deleteheader-v3.js

---
+++ deleteheader-v3.js
@@ -0,0 +1,18 @@
+
+const version = 'V1.0.3';
+
+
+function removeHeader(headers, key) {
+    const lowerKey = key.toLowerCase();
+    if (headers[key]) {
+        delete headers[key];
+    }
+    if (headers[lowerKey]) {
+        delete headers[lowerKey];
+    }
+}
+var modifiedHeaders = $request.headers;
+removeHeader(modifiedHeaders, "If-None-Match");
+removeHeader(modifiedHeaders, "X-RevenueCat-ETag");
+removeHeader(modifiedHeaders, "X-Headers-Hash");
+$done({headers: modifiedHeaders});
