8839822222a6041959aca2b478728b5467d58048
aleotoidayy <luannguyen6695@gmail.com>
2025-11-04 13:00:09 +0700

    Create xterminal.js

---
+++ xterminal.js
@@ -0,0 +1,76 @@
+var aleoo = {
+  "environment": "Production",
+  "receipt": {
+    "receipt_type": "Production",
+    "app_item_id": 1544728400,
+    "receipt_creation_date": "2025-01-01 00:00:00 Etc/GMT",
+    "bundle_id": "com.septudio.SSHClientLite",
+    "original_purchase_date": "2025-01-01 00:00:00 Etc/GMT",
+    "in_app": [
+      {
+        "quantity": "1",
+        "purchase_date_ms": "1735689600000",
+        "expires_date": "2099-01-01 00:00:00 Etc/GMT",
+        "expires_date_pst": "2098-12-31 16:00:00 America/Los_Angeles",
+        "is_in_intro_offer_period": "false",
+        "transaction_id": "190001678979391",
+        "is_trial_period": "true",
+        "original_transaction_id": "190001678979391",
+        "purchase_date": "2025-01-01 00:00:00 Etc/GMT",
+        "product_id": "xterminal.pro2",
+        "original_purchase_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+        "in_app_ownership_type": "PURCHASED",
+        "original_purchase_date_ms": "1735689600000",
+        "web_order_line_item_id": "190000756628820",
+        "expires_date_ms": "4070908800000",
+        "purchase_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+        "original_purchase_date": "2025-01-01 00:00:00 Etc/GMT"
+      }
+    ],
+    "adam_id": 1544728400,
+    "receipt_creation_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+    "request_date": "2025-01-01 00:00:00 Etc/GMT",
+    "request_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+    "version_external_identifier": 854046885,
+    "request_date_ms": "1735689600000",
+    "original_purchase_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+    "application_version": "186",
+    "original_purchase_date_ms": "1735689600000",
+    "receipt_creation_date_ms": "1735689600000",
+    "original_application_version": "186",
+    "download_id": null
+  },
+  "pending_renewal_info": [
+    {
+      "product_id": "xterminal.pro2",
+      "original_transaction_id": "190001678979391",
+      "auto_renew_product_id": "xterminal.pro2",
+      "auto_renew_status": "1"
+    }
+  ],
+  "status": 0,
+  "latest_receipt_info": [
+    {
+      "quantity": "1",
+      "purchase_date_ms": "1735689600000",
+      "expires_date": "2099-01-01 00:00:00 Etc/GMT",
+      "expires_date_pst": "2098-12-31 16:00:00 America/Los_Angeles",
+      "is_in_intro_offer_period": "false",
+      "transaction_id": "190001678979391",
+      "is_trial_period": "true",
+      "original_transaction_id": "190001678979391",
+      "purchase_date": "2025-01-01 00:00:00 Etc/GMT",
+      "product_id": "xterminal.pro2",
+      "original_purchase_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+      "in_app_ownership_type": "PURCHASED",
+      "subscription_group_identifier": "20871436",
+      "original_purchase_date_ms": "1735689600000",
+      "web_order_line_item_id": "190000756628820",
+      "expires_date_ms": "4070908800000",
+      "purchase_date_pst": "2024-12-31 16:00:00 America/Los_Angeles",
+      "original_purchase_date": "2025-01-01 00:00:00 Etc/GMT"
+    }
+  ]
+};
+
+$done({ body: JSON.stringify(aleoo) });
