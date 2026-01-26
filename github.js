4f883a17222eba152e5087ebdd3475a40eae2463
aleotoidayy <luannguyen6695@gmail.com>
2026-01-26 09:04:44 +0700

    aleoo

---
+++ github.js
@@ -0,0 +1,12 @@
+let obj = JSON.parse($response.body);
+if (obj.data && obj.data.viewer) {
+  obj.data.viewer.isProPlan = true;
+  obj.data.viewer.isEmployee = true;
+  obj.data.viewer.hasAppleIapSubscription = true;
+
+  $done({
+    body: JSON.stringify(obj)
+  });
+} else {
+  $done({});
+}
