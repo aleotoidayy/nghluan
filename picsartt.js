642a5a131b693ff2429d49c160bff4c66e8ad2a3
aleotoidayy <luannguyen6695@gmail.com>
2026-01-28 12:08:35 +0700

    aleoo

---
+++ picsartt.js
@@ -0,0 +1,11 @@
+const aleoo = {
+  "response": {
+    "totalStorageSizeInBytes": 0,
+    "storageLimitSizeInBytes": 1099511627776, 
+    "foldersCountLimit": -1,
+    "totalFoldersCount": 0
+  },
+  "status": "success"
+};
+
+$done({ body: JSON.stringify(aleoo) });
