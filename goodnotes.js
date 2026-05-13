3f03ef94604fc8e40dcfb80903f7632bf886ff0f
aleotoidayy <luannguyen6695@gmail.com>
2026-05-13 13:54:14 +0700

    aleoo

--- goodnotes.js
+++ goodnotes.js
@@ -1,38 +1,38 @@
-const aleo = {};
-const aleoo = JSON.parse(typeof $response != "undefined" && $response.body || "{}");
+var aleoo = JSON.parse($response.body);
 
-const aleooo = "apple_access";
-const aleooooo = "crossplatform_access";
-const product_id = "com.goodnotes.gn6_one_time_unlock_3999";
+aleoo.subscriber.entitlements = {
+    "apple_access": {
+        "expires_date": "2099-01-01T09:09:09Z",
+        "product_identifier": "com.goodnotes.pro_7dt_1y_3599",
+        "purchase_date": "2026-01-01T02:33:33Z"
+    },
+    "crossplatform_access": {
+        "expires_date": "2099-01-01T09:09:09Z",
+        "product_identifier": "com.goodnotes.pro_7dt_1y_3599",
+        "purchase_date": "2026-01-01T02:33:33Z"
+    },
+    "pro_access": {
+        "expires_date": "2099-01-01T09:09:09Z",
+        "product_identifier": "com.goodnotes.pro_7dt_1y_3599",
+        "purchase_date": "2026-01-01T02:33:33Z"
+    },
+    "gnc_access": {
+        "expires_date": "2099-01-01T09:09:09Z",
+        "product_identifier": "com.goodnotes.pro_7dt_1y_3599",
+        "purchase_date": "2026-01-01T02:33:33Z"
+    }
+};
 
-if (typeof $response == "undefined") {
+aleoo.subscriber.original_purchase_date = "2026-01-01T03:33:33Z";
 
-    delete $request.headers["x-revenuecat-etag"];
-    delete $request.headers["X-RevenueCat-ETag"];
-    aleo.headers = $request.headers;
+aleoo.subscriber.subscriptions = {
+    "com.goodnotes.pro_7dt_1y_3599": {
+        "expires_date": "2099-01-01T09:09:09Z",
+        "original_purchase_date": "2026-01-01T02:33:33Z",
+        "purchase_date": "2026-01-01T02:33:33Z",
+        "ownership_type": "PURCHASED",
+        "store": "app_store"
+    }
+};
 
-} else if (aleoo && aleoo.subscriber) {
-    if (!aleoo.subscriber.subscriptions) aleoo.subscriber.subscriptions = {};
-    if (!aleoo.subscriber.entitlements) aleoo.subscriber.entitlements = {};
-
-    const data = {
-        "purchase_date": "2025-01-01T09:09:09Z"
-    };
-
-    aleoo.subscriber.subscriptions[product_id] = {
-        "original_purchase_date": "2025-01-01T09:09:09Z",
-        "purchase_date": "2025-01-01T09:09:09Z",
-        "store": "app_store",
-        "ownership_type": "PURCHASED"
-    };
-
-    aleoo.subscriber.entitlements[aleooo] = JSON.parse(JSON.stringify(data));
-    aleoo.subscriber.entitlements[aleooooo] = JSON.parse(JSON.stringify(data));
-
-    aleoo.subscriber.entitlements[aleooo].product_identifier = product_id;
-    aleoo.subscriber.entitlements[aleooooo].product_identifier = product_id;
-
-    aleo.body = JSON.stringify(aleoo);
-}
-
-$done(aleo);
+$done({ body: JSON.stringify(aleoo) });
