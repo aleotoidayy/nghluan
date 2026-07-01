05361185c7f46af352255cee7d78adf761fd0972
aleotoidayy <luannguyen6695@gmail.com>
2026-07-01 19:29:32 +0700

    aleoo

--- photoroomm.js
+++ photoroomm.js
@@ -1,9 +1,12 @@
 let body = $response.body;
+
 body = body
   
     .replace(/"validation_status":"[^"]*"/g, '"validation_status":"VERIFYING"')
     .replace(/"error_message":"[^"]*"/g, '"error_message":null')
     .replace(/"is_eligible_for_assigning_revenuecat_subscription":(true|false)/g, '"is_eligible_for_assigning_revenuecat_subscription":true')
-    .replace(/"badgeText":"[^"]*"/g, '"badgeText":"Ultra"');
+    .replace(/"badgeText":"[^"]*"/g, '"badgeText":"Ultra"')
+    .replace(/"aiCreditsPercentageRemaining":[\d.]+/g, '"aiCreditsPercentageRemaining":100.0')
+    .replace(/"resetDateTime":"[^"]*"/g, '"resetDateTime":"2099-01-01T00:00:00Z"');
 
 $done({body});
